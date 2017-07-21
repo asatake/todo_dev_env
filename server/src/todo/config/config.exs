7891d8e7fc56163a77a87163102fbe8ba60edc80
asatake <manome@axelmark.co.jp>
2017-07-21 19:20:42 +0900

    開発環境構築完了?

--- server/src/todo/config/config.exs
+++ server/src/todo/config/config.exs
@@ -12,6 +12,7 @@ config :todo, Todo.Repo,
 config :todo,
   ecto_repos: [Todo.Repo]
 
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
